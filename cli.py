662e3868d1f4b1767ddf680b6cdbb56c8b3ced71
Junaid Ahamed <junuhammad2@gmail.com>
2026-06-08 23:16:49 +0530

    feat(cli): Added exception handling to show if placement of order fails

--- cli.py
+++ cli.py
@@ -16,47 +16,52 @@ def main(
     quantity,
     price
 ):
-    service = OrderService()
-    order_type = order_type.upper()
-    if order_type == "MARKET":
-        response = service.place_market_order(
-            symbol=symbol,
-            side=side,
-            quantity=quantity
-        )
-    elif order_type == "LIMIT":
-        if price is None:
-            raise click.ClickException(
-                "LIMIT orders require --price"
+    try: 
+        service = OrderService()
+        order_type = order_type.upper()
+        if order_type == "MARKET":
+            response = service.place_market_order(
+                symbol=symbol,
+                side=side,
+                quantity=quantity
             )
-        response = service.place_limit_order(
-            symbol=symbol,
-            side=side,
-            quantity=quantity,
-            price=price
-        )
-    else:
-        raise click.ClickException(
-            "Order type must be MARKET or LIMIT"
-        )
+        elif order_type == "LIMIT":
+            if price is None:
+                raise click.ClickException(
+                    "LIMIT orders require --price"
+                )
+            response = service.place_limit_order(
+                symbol=symbol,
+                side=side,
+                quantity=quantity,
+                price=price
+            )
+        else:
+            raise click.ClickException(
+                "Order type must be MARKET or LIMIT"
+            )
         
-    click.echo("\nOrder Request Summary")
-    click.echo("---------------------")
-    click.echo(f"Symbol: {symbol}")
-    click.echo(f"Side: {side}")
-    click.echo(f"Type: {order_type}")
-    click.echo(f"Quantity: {quantity}")
-    
-    click.echo("\nOrder Response")
-    click.echo("--------------")
-    click.echo(f"Order ID: {response['orderId']}")
-    click.echo(f"Status: {response['status']}")
-    click.echo(f"Executed Qty: {response['executedQty']}")
-    
-    if response.get("avgPrice"):
-        click.echo(
-            f"Average Price: {response['avgPrice']}"
-        )
+        if response["status"] in ["FILLED", "NEW", "PARTIALLY_FILLED"]:
+            click.echo("\nOrder Has Been Placed Successfully")
         
+        click.echo("\nOrder Request Summary")
+        click.echo("---------------------")
+        click.echo(f"Symbol: {symbol}")
+        click.echo(f"Side: {side}")
+        click.echo(f"Type: {order_type}")
+        click.echo(f"Quantity: {quantity}")
+        
+        click.echo("\nOrder Response")
+        click.echo("--------------")
+        click.echo(f"Order ID: {response['orderId']}")
+        click.echo(f"Status: {response['status']}")
+        click.echo(f"Executed Qty: {response['executedQty']}")
+        
+        if response.get("avgPrice"):
+            click.echo(
+                f"Average Price: {response['avgPrice']}"
+            )
+    except Exception as e:
+        click.echo(f"\nOrder Failed : {e}")
 if __name__ == "__main__":
     main()
