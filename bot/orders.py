a790ce4a17b31a0766fda0bdfcf548b12328d8d1
Junaid Ahamed <junuhammad2@gmail.com>
2026-06-08 22:32:20 +0530

    feat(orders): Added and filled class OrderService

--- bot/orders.py
+++ bot/orders.py
@@ -8,3 +8,34 @@ from bot.validators import (
 )
 from bot.client import BinanceFuturesClient
 from bot.logging_config import setup_logger
+
+class OrderService:
+    def __init__(self):
+        self.client = BinanceFuturesClient()
+        self.logger = setup_logger()
+    def place_market_order(
+        self,
+        symbol,
+        side,
+        quantity
+    ):
+        symbol = validate_symbol(symbol)
+        side = validate_side(side)
+        quantity = validate_quantity(quantity)
+
+        self.logger.info(
+            f"Market Order Requested: "
+            f"{symbol} {side} {quantity}"
+        )
+        
+        response = self.client.create_market_order(
+            symbol=symbol,
+            side=side,
+            quantity=quantity
+        )
+        
+        self.logger.info(
+            f"Order created successfully: "
+            f"{response['orderId']}"
+        )
+        return response
